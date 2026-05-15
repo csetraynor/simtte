dd9e5efe56b90f32c129692530f1dbc92761389e
carlos-serratraynor <carlos.serratraynor@astrazeneca.com>
2026-05-15 18:19:13 -0400

    fix: pipe usage

--- R/simtte-package.R
+++ R/simtte-package.R
@@ -3,7 +3,7 @@
 "_PACKAGE"
 
 #' @import mrgsolve
-#' @importFrom dplyr %>% group_by group_split bind_rows mutate filter
+#' @importFrom dplyr group_by group_split bind_rows mutate filter
 #'   select distinct inner_join left_join ungroup tibble
 #' @importFrom stats runif
 NULL
