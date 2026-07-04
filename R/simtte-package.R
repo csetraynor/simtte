576c1dff11d0c5c22c10c8fc17ba21362d8df08d
Carlos Traynor <carlostraynor@nievess-MacBook-Air.local>
2026-07-04 09:48:42 +0200

    Release version 1.0.1

--- R/simtte-package.R
+++ R/simtte-package.R
@@ -3,7 +3,6 @@
 "_PACKAGE"
 
 #' @import mrgsolve
-#' @importFrom dplyr group_by group_split bind_rows mutate filter
-#'   select distinct inner_join left_join ungroup tibble
+#' @importFrom dplyr group_by group_split bind_rows mutate filter select distinct inner_join left_join ungroup tibble
 #' @importFrom stats runif
 NULL
